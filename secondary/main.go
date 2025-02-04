60ce6a05cbca1a943d11732ec3eef96ce5477bed
Gvaldz <233415@ids.upchiapas.edu.mx>
2025-02-04 12:32:44 -0600

    add: Se implementa short polling

--- secondary/main.go
+++ secondary/main.go
@@ -3,11 +3,44 @@ package main
 import (
 	"net/http"
 	"io/ioutil"
+	"time"
+	"log"
 )
 
+var lastData []byte
+
 func main() {
+	go pollServerPrincipal()
+
 	http.HandleFunc("/users", handleUsers)
+	log.Println("Servidor secundario escuchando en :8081...")
 	http.ListenAndServe(":8081", nil)
+}
+
+func pollServerPrincipal() {
+	for {
+		resp, err := http.Get("http://localhost:8080/users")
+		if err != nil {
+			log.Printf("Error al consultar el servidor principal: %v\n", err)
+			time.Sleep(5 * time.Second) 
+			continue
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			log.Printf("Error al leer la respuesta del servidor principal: %v\n", err)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		if string(body) != string(lastData) {
+			log.Println("Datos actualizados en el servidor principal")
+			lastData = body
+		}
+
+		time.Sleep(5 * time.Second)
+	}
 }
 
 func handleUsers(w http.ResponseWriter, r *http.Request) {
@@ -16,20 +49,7 @@ func handleUsers(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	resp, err := http.Get("http://localhost:8080/users")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(body)
+	w.Write(lastData)
 }
